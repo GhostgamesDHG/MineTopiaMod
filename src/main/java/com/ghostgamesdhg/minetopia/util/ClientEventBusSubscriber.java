59d7b9b560fe252108ac880675c90935ffe141bd
GhostgamesDHG <dgcooldjd@outlook.com>
2021-07-08 18:31:18 +0200

    Initial Commit: All the files of the MineTopia Mod.

--- src/main/java/com/ghostgamesdhg/minetopia/util/ClientEventBusSubscriber.java
+++ src/main/java/com/ghostgamesdhg/minetopia/util/ClientEventBusSubscriber.java
@@ -1,6 +1,12 @@
 package com.ghostgamesdhg.minetopia.util;
 
 import com.ghostgamesdhg.minetopia.MinetopiaExtra;
+import com.ghostgamesdhg.minetopia.client.screen.GreenBagScreen;
+import com.ghostgamesdhg.minetopia.init.ContainerTypesInit;
+import com.ghostgamesdhg.minetopia.init.ModBlocks;
+import net.minecraft.client.gui.ScreenManager;
+import net.minecraft.client.renderer.RenderType;
+import net.minecraft.client.renderer.RenderTypeLookup;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
@@ -11,6 +17,7 @@ public class ClientEventBusSubscriber {
 
     @SubscribeEvent
     public static void clientSetup(FMLClientSetupEvent event) {
-
+        ScreenManager.registerFactory(ContainerTypesInit.GREEN_BAG_CONTAINER_TYPE.get(), GreenBagScreen::new);
+        RenderTypeLookup.setRenderLayer(ModBlocks.OVEN.get(), RenderType.getTranslucent());
     }
 }
