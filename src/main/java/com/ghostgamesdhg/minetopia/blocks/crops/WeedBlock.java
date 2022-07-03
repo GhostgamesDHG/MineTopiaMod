f81b2b987cc0f877a0f54052b2ff3b8e03804532
DGcoo <dgcooldjd@outlook.com>
2022-07-03 14:50:39 +0200

    Updated/Added/Removed files...

---
+++ src/main/java/com/ghostgamesdhg/minetopia/blocks/crops/WeedBlock.java
@@ -0,0 +1,58 @@
+package com.ghostgamesdhg.minetopia.blocks.crops;
+
+import com.ghostgamesdhg.minetopia.init.ModFood;
+import net.minecraft.block.Block;
+import net.minecraft.block.BlockState;
+import net.minecraft.block.Blocks;
+import net.minecraft.block.CropsBlock;
+import net.minecraft.util.IItemProvider;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.shapes.VoxelShape;
+import net.minecraft.world.IBlockReader;
+import net.minecraft.world.IWorldReader;
+import net.minecraft.world.World;
+
+import java.util.Random;
+
+public class WeedBlock extends CropsBlock {
+    private static final VoxelShape[] SHAPE_BY_AGE = new VoxelShape[]{
+            Block.makeCuboidShape(0.0D, 0.0D, 0.0D, 16.0D, 2.0D, 16.0D), //0 (GrowStage)
+            Block.makeCuboidShape(0.0D, 0.0D, 0.0D, 16.0D, 4.0D, 16.0D), //1
+            Block.makeCuboidShape(0.0D, 0.0D, 0.0D, 16.0D, 6.0D, 16.0D), //2
+            Block.makeCuboidShape(0.0D, 0.0D, 0.0D, 16.0D, 8.0D, 16.0D), //3
+            Block.makeCuboidShape(0.0D, 0.0D, 0.0D, 16.0D, 10.0D, 16.0D), //4
+            Block.makeCuboidShape(0.0D, 0.0D, 0.0D, 16.0D, 12.0D, 16.0D), //5
+            Block.makeCuboidShape(0.0D, 0.0D, 0.0D, 16.0D, 14.0D, 16.0D), //6
+            Block.makeCuboidShape(0.0D, 0.0D, 0.0D, 16.0D, 16.0D, 16.0D)}; //7
+
+    public WeedBlock(Properties builder) {
+        super(builder);
+    }
+
+    @Override
+    public boolean isValidPosition(BlockState state, IWorldReader worldIn, BlockPos pos) {
+        return (worldIn.getLightSubtracted(pos, 0) >= 12 && super.isValidPosition(state, worldIn, pos));
+    }
+
+    @Override
+    public int getMaxAge() {
+        return 7;
+    }
+
+    @Override
+    protected IItemProvider getSeedsItem() {
+        return ModFood.WEED.get();
+    }
+
+    @Override
+    public boolean canUseBonemeal(World worldIn, Random rand, BlockPos pos, BlockState state) {
+        return false;
+    }
+
+    @Override
+    protected boolean isValidGround(BlockState state, IBlockReader worldIn, BlockPos pos) {
+        if ((worldIn.getBlockState(new BlockPos(0, 3, 0))).getBlock() == Blocks.DIAMOND_BLOCK) {
+        }
+        return state.isIn(Blocks.PODZOL);
+    }
+}
